65cbfef0660475fce0f634d378d55fa29a55e90e
Brandon Zenbook <tangbrandonk@gmail.com>
2023-12-19 17:47:46 -0800

    Refactor piece component and chess board setup

--- src/app/components/piece.tsx
+++ src/app/components/piece.tsx
@@ -1,61 +1,75 @@
 import React, { useRef } from "react";
-import Draggable, { DraggableEvent, DraggableData } from 'react-draggable'; // Add this import statement
+import Draggable, { DraggableEvent, DraggableData } from "react-draggable"; // Add this import statement
 
 interface PieceProps {
-    pieceType: string;
-    pieceColor: string;
+  pieceType: string;
+  pieceColor: string;
+  x: number;
+  y: number;
 }
 
-export default function Piece({pieceType, pieceColor}: PieceProps) {
-    var pieceName: string = pieceColor == "w" ? "white" : "black";
-    const pieceRef = useRef<HTMLButtonElement | null>(null);
-    
-    switch (pieceType) {
-        case "p":
-            pieceName += "pawn";
-            break;
-        case "r":
-            pieceName += "rook";
-            break;
-        case "n":
-            pieceName += "knight";
-            break;
-        case "b":
-            pieceName += "bishop";
-            break;
-        case "q":
-            pieceName += "queen";
-            break;
-        case "k":
-            pieceName += "king";
-            break;
-        default:
-            console.log("Invalid piece type");
-            break;
-    }
-    
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        console.log("Piece clicked");
-    }
+export default function Piece({ pieceType, pieceColor, x, y }: PieceProps) {
+  var pieceName: string = pieceColor == "w" ? "white" : "black";
+  const pieceRef = useRef<HTMLButtonElement | null>(null);
 
-    const handleStop = (e: DraggableEvent, data: DraggableData) => {
-        const squareSize = 100;
-        const x = Math.round(data.x / squareSize) * squareSize;
-        const y = Math.round(data.y / squareSize) * squareSize;
-        console.log("Piece snapped to square:", x, y);
-        pieceRef.current!.style.transform = `translate(${x}px, ${y}px)`;
-    };
+  switch (pieceType) {
+    case "p":
+      pieceName += "pawn";
+      break;
+    case "r":
+      pieceName += "rook";
+      break;
+    case "n":
+      pieceName += "knight";
+      break;
+    case "b":
+      pieceName += "bishop";
+      break;
+    case "q":
+      pieceName += "queen";
+      break;
+    case "k":
+      pieceName += "king";
+      break;
+    default:
+      console.log("Invalid piece type");
+      break;
+  }
 
-    return(
-        <Draggable nodeRef={pieceRef} onStop={handleStop}>
-            <button
-                ref={pieceRef}
-                className="w-full h-full grid place-items-center hover:scale-[105%]"
-                onClick={handleClick}
-            >
-                <img src={`/${pieceName}.png`} alt={pieceName} className="mr-[4px]" />
-            </button>
-        </Draggable>
-    );
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Piece clicked");
+  };
+
+  const handleStop = (e: DraggableEvent, data: DraggableData) => {
+    const squareSize = 64;
+    const newX = Math.round(data.x / squareSize) * squareSize;
+    const newY = Math.round(data.y / squareSize) * squareSize;
+
+    if (
+      newX + 64 * x < 0 ||
+      448 - (newY + 64 * y) < 0 ||
+      newX + 64 * x >= squareSize * 8 ||
+      448 - (newY + 64 * y) >= squareSize * 8
+    ) {
+      console.error("Piece dragged off the board");
+      pieceRef.current!.style.transform = `translate(${x / squareSize}px, ${
+        y / squareSize
+      }px)`;
+      pieceRef.current!.style.transition = "transform 0.5s";
+      return;
+    } else
+      pieceRef.current!.style.transform = `translate(${newX}px, ${newY}px)`;
+  };
+  return (
+    <Draggable nodeRef={pieceRef} onStop={handleStop}>
+      <button
+        ref={pieceRef}
+        className="w-full h-full grid place-items-center hover:scale-[105%]"
+        onClick={handleClick}
+      >
+        <img src={`/${pieceName}.png`} alt={pieceName} className="mr-[4px]" />
+      </button>
+    </Draggable>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,85 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import Piece from './components/piece';
+import React, { useEffect, useState } from "react";
+import Piece from "./components/piece";
 
-type PieceStart = [color: 'b' | 'w', type: 'r' | 'n' | 'b' | 'q' | 'k' | 'p']
+type PieceStart = [color: "b" | "w", type: "r" | "n" | "b" | "q" | "k" | "p"];
 
-function setup(){
+function setup() {
   const piece_types = ["rnbqkbnr", "pppppppp"];
   let board: Record<string, PieceStart> = {};
-  ['b','w'].flatMap((color) => {
-    const row = color === 'w' ? 1 : 8;
-    const pawn_row = color === 'w' ? 2 : 7;
-    const pieces = piece_types.map((piece_type) => piece_type.split(''));
+  ["b", "w"].flatMap((color) => {
+    const row = color === "w" ? 1 : 8;
+    const pawn_row = color === "w" ? 2 : 7;
+    const pieces = piece_types.map((piece_type) => piece_type.split(""));
     pieces.forEach((piece_type, i) => {
       piece_type.forEach((piece, j) => {
         const x = j + 1;
         const y = i === 0 ? row : pawn_row;
         board[`${x}${y}`] = [color, piece] as PieceStart;
-      })
-    })
-  })
-  console.log(board)
-  return board
+      });
+    });
+  });
+  console.log(board);
+  return board;
+}
+
+function useBoard() {
+  const [board, setBoard] = useState<Record<string, PieceStart>>({});
+
+  const createSetPiece = (x: number, y: number) => {
+    // toolkit for each piece
+    const move = (newX: number, newY: number) => {
+      setBoard((prevBoard) => {
+        const newBoard = { ...prevBoard };
+        const piece = newBoard[`${x + 1}${8 - y}`];
+        delete newBoard[`${x + 1}${8 - y}`];
+        newBoard[`${newX + 1}${8 - newY}`] = piece;
+        return newBoard;
+      });
+    };
+    return { move };
+  };
+  return { board, setBoard, createSetPiece };
 }
 
 export default function ChessBoard() {
   const boardSize = 8;
-  const [board, setBoard] = useState<Record<string, PieceStart>>({});
+  const { board, setBoard, createSetPiece } = useBoard();
 
   useEffect(() => {
     setBoard(setup());
   }, []);
 
-  return <div className="grid place-items-center w-full min-h-screen" >
-      <div className="grid grid-cols-8 aspect-square">{
-        [...Array(boardSize).keys()].map((y: number) => 
+  return (
+    <div className="grid place-items-center w-full min-h-screen">
+      <div className="grid grid-cols-8 aspect-square">
+        {[...Array(boardSize).keys()].map((y: number) =>
           [...Array(boardSize).keys()].map((x: number) => {
-              const piece = board[`${x + 1}${boardSize - y}`]; // Flip the y coordinate
-              return <div key={`${x}${y}`} className={`${(x + y) % 2 ? 
-              "bg-white" : "bg-[#0000FF]"} aspect-square w-10 md:w-16 border-4 border-transparent hover:border-black`}
+            const piece = board[`${x + 1}${boardSize - y}`]; // Flip the y coordinate
+            const { move } = createSetPiece(x, y);
+
+            return (
+              <div
+                key={`${x}${y}`}
+                className={`${
+                  (x + y) % 2 ? "bg-white" : "bg-[#0000FF]"
+                } aspect-square w-10 md:w-16 border-4 border-transparent hover:border-black`}
               >
-                {piece && <Piece pieceType={piece[1]} pieceColor={piece[0]} />}
+                {piece && (
+                  <Piece
+                    pieceType={piece[1]}
+                    pieceColor={piece[0]}
+                    // move = {move}
+                    x={x}
+                    y={y}
+                  />
+                )}
               </div>
-            }
-          )
-        )
-      }
+            );
+          })
+        )}
+      </div>
     </div>
-  </div>
+  );
 }
